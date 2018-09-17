f76d93ac5b0ca60e1585983fcbade9e8565725e2
guruguruman <katot.dev@gmail.com>
2018-09-17 15:26:46 +0900

    Added test to run.

---
+++ test/readable-kanji-balance-test.js
@@ -0,0 +1,42 @@
+"use strict";
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+
+// rule
+const rule = require("../src/readable-kanji-balance");
+
+// ruleName, rule, { valid, invalid }
+tester.run("rule", rule, {
+  valid: [
+    // no problem
+    "漢字があまりに多いと読みづらくなる傾向があると言われています。",
+    "可読率低下。",
+  ],
+  invalid: [
+    // single match.
+    {
+      text: "漢字が余りにも多いと可読率低下を招く。",
+      errors: [{
+        message: "1文における漢字比率が「53%」と多くの漢字が含まれています。（目標比率: 40%）漢字が多すぎる場合、読みにくさにつながることがあります。",
+        line: 1,
+        column: 1
+      }]
+    },
+    // multiple match
+    {
+      text: `漢字が余りにも多いと可読率低下を招く。
+
+但し漢字は優秀な表現。`,
+      errors: [{
+          message: "1文における漢字比率が「53%」と多くの漢字が含まれています。（目標比率: 40%）漢字が多すぎる場合、読みにくさにつながることがあります。",
+          line: 1,
+        },
+        {
+          message: "1文における漢字比率が「64%」と多くの漢字が含まれています。（目標比率: 40%）漢字が多すぎる場合、読みにくさにつながることがあります。",
+          line: 3,
+        }
+      ]
+    },
+
+  ]
+});
